9041c2ceec1218f74ff5b5f8f7cd04fbfa8216e1
Akshada-sable07 <sableakshada100@gmail.com>
2025-04-06 18:51:32 +0530

    Add files via upload

---
+++ Video_Steganography.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video_steganography;
+
+/**
+ *
+ * @author welcome
+ */
+public class Video_Steganography {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        LoginFrame lf=new LoginFrame();
+        lf.setVisible(true);
+        lf.setSize(950,600);
+        lf.setLocation(500, 100);
+    }
+    
+}
